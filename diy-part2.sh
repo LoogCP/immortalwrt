384d8ef9df0302a5d3e4224471b91dc12d27d5b0
LoogCP <lp18883370076@gmail.com>
2025-11-10 15:24:09 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,11 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+#修改默认IP
+sed -i 's/192.168.1.1/10.10.1.1/g' package/base-files/files/bin/config_generate
+
+#修改默认shell
+sed -i 's/\/bin\/ash/\/usr\/bin\/fish/g' package/base-files/files/etc/passwd
 
 # 删除 golang 语言包
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
@@ -16,6 +21,10 @@
 
 # 删除 feeds 中的 v2ray-geodata 包（适用于 openwrt-22.03 和 master）
 # rm -rf feeds/packages/net/v2ray-geodata
+
+#切换dev分支hp
+rm -rf  feeds/luci/applications/luci-app-homeproxy
+git clone -b dev https://github.com/immortalwrt/homeproxy.git package/homrproxy
 
 # 添加argon、infinityfreedom和alpha主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
